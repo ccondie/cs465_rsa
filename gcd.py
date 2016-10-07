ad8d85d41133197339f29cabcad4317bec07cf5a
Clayton Condie <clayton.condie@gmail.com>
2016-10-07 11:48:18 -0600

    completed extended euclid algorithm

--- gcd.py
+++ gcd.py
@@ -8,5 +8,22 @@ def euclid(phi, e):
         return euclid(e, modex(phi, 1, e))
 
 
-def extended_euclid(a, b):
-    pass
+def ext_euc_calc_d(phi_n, e):
+    s = 0
+    t = 1
+    r = e
+
+    old_s = 1
+    old_t = 0
+    old_r = phi_n
+
+    while r != 0:
+        quotient = int(old_r / r)
+        old_r, r = r, old_r - quotient * r
+        old_s, s = s, old_s - quotient * s
+        old_t, t = t, old_t - quotient * t
+
+    if old_t < 0:
+        return phi_n + old_t
+    else:
+        return old_t
--- run.py
+++ run.py
@@ -1,20 +1,29 @@
 from Crypto.Util import number
 from gcd import euclid
+from gcd import ext_euc_calc_d
 import os
 
 if __name__ == '__main__':
-    e = 7
-    p = number.getPrime(512)
-    q = number.getPrime(512)
-    # p = 17
-    # q = 11
+    e = 3
+    # p = number.getPrime(512)
+    # q = number.getPrime(512)
+    p = 5
+    q = 11
     n = p * q
     phi = (p - 1) * (q - 1)
 
     print('p:  \t' + str(p))
     print('q:  \t' + str(q))
+    print('e:  \t' + str(e))
     print('n:  \t' + str(n))
     print('phi:\t' + str(phi))
+    print()
 
     gcd_phi_e = euclid(phi, e)
     print('gcd:\t' + str(gcd_phi_e))
+    print()
+
+    d_val = ext_euc_calc_d(phi, e)
+
+    print()
+    print('d:  \t' + str(d_val))
